b484be37f27556fa4eb518471577803c7e85526e
Damiano Vincenzo Coppola <damiano.coppola03@gmail.com>
2025-03-17 03:45:37 +0100

    fix:message and device already in response.

--- BoatManager/backend/main.py
+++ BoatManager/backend/main.py
@@ -288,7 +288,7 @@ def register():
 
         db.session.add(new_device)
         db.session.commit()
-    return jsonify({"message": "Device added", "device": respons}), 201
+    return jsonify({"respons": respons}), 201
 
 
 @app.route("/delete/<deviceid>")
