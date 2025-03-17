914e74eb5be895eee3cf7fd61ac9181ab2bc36e3
Damiano Vincenzo Coppola <damiano.coppola03@gmail.com>
2025-03-17 03:40:02 +0100

    Fix:Change url "/register/" to "/register" and fixed response

---
+++ BoatManager/front-end/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { NavBar } from "../../components";
+import { ContentWrapper } from "../../components/Wrapper/style";
+
+const LoginPage: React.FC = () => {
+	return (
+		<>
+
+			<ContentWrapper>
+				<NavBar />
+
+			</ContentWrapper>
+		</>
+	)
+}
+
+export default LoginPage;
